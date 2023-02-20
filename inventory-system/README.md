6ba96b01b70309ebfe2cdad9bde5b7aeb0254191
dojinyou <djyou128@gmail.com>
2023-02-20 18:01:00 +0900

    project set up

---
+++ inventory-system/README.md
@@ -0,0 +1,7 @@
+# 재고 시스템 
+
+## 실행하기
+```shell
+cd infra
+docker compose -p inventory_system_db up -d
+```
---
+++ inventory-system/src/main/kotlin/com/dojinyou/inflearn/inventorysystem/InventorySystemApplication.kt
@@ -0,0 +1,11 @@
+package com.dojinyou.inflearn.inventorysystem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InventorySystemApplication
+
+fun main(args: Array<String>) {
+    runApplication<InventorySystemApplication>(*args)
+}
